464033506aa7e486eaab6184cfef8e6fc864dd1e
CyrusWarner <cyruswarner27@gmail.com>
2021-08-18 20:47:20 -0500

    Added CRUD functionality to NotesController.

---
+++ Bug Tracker/Controllers/NotesController.cs
@@ -0,0 +1,70 @@
+﻿using Bug_Tracker.Data;
+using Bug_Tracker.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Bug_Tracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+        public NotesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: api/<NotesController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var notes = _context.Notes;
+            return Ok(notes);
+        }
+
+        // GET api/<NotesController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var note = _context.Notes.FirstOrDefault(note => note.NotesId == id);
+            return Ok(note);
+        }
+
+        // POST api/<NotesController>
+        [HttpPost]
+        public IActionResult Post([FromBody]Notes value)
+        {
+            _context.Notes.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        // PUT api/<NotesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody]Notes value)
+        {
+            var note = _context.Notes.FirstOrDefault(note => note.NotesId == id);
+            note.Title = value.Title;
+            note.Description = value.Description;
+            note.UserId = value.UserId;
+            note.BoardId = value.BoardId;
+            return Ok(value);
+
+        }
+
+        // DELETE api/<NotesController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var note = _context.Notes.FirstOrDefault(note => note.NotesId == id);
+            _context.Remove(note);
+            _context.SaveChanges();
+            return Ok();
+        }
+    }
+}
