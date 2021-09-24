57aad53db0518d384cdfad207d9ba5d7601223ef
CyrusWarner <cyruswarner27@gmail.com>
2021-09-24 15:09:14 -0500

    Moved BoardController logic to Business layer.

---
+++ Bug Tracker/Interfaces/IBoardRepository.cs
@@ -0,0 +1,19 @@
+﻿using Bug_Tracker.Models;
+using System.Collections;
+using System.Threading.Tasks;
+
+namespace Bug_Tracker.Interfaces
+{
+    public interface IBoardRepository
+    {
+        Task<bool>BoardExists(int boardId);
+        Task<IEnumerable>GetAllBoards(int id);
+        Task<IEnumerable>GetInvitedBoards(int userId);
+        Task<UserBoard>GetBoard(int id, int userId);
+        Task<bool>AddNewBoard(Board board);
+        Task<bool>AddBoardToUserBoard(int userId, Board board);
+        Task<bool>AcceptBoardInvite(Board board, int userId);
+        Task<bool>RemoveBoardRelationship(int boardId, int userId);
+
+    }
+}
